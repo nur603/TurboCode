b1082e82377aa73add0c4d3db925c274db63f5a3
Nur <96_nur@mail.ru>
2021-04-17 16:01:20 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,57 @@
+# Hello World
+# print("Hello Elon Mars")
+# '''
+# print("Error") 
+# '''
+# print("Salem")
+
+#data types
+
+# string
+# a = 'asdasd'
+# b = "asdasdasd"
+# c = '''asdasdasd'''
+
+# #int
+# a = 1123
+
+# # float
+# a = 23.123
+
+# a1 = [1,2,3, '223']
+
+# d = {
+#   'asd': 123,
+#   '23': 233
+# }
+
+# # boolean
+# a = True
+# a = False
+
+# print(type()) узнать тип
+
+# a = '123'  преобразовывать 
+# a = str(a)
+# print(a)
+# print(type(a))
+# x = range (6)
+#print("x")
+
+# a = 2+2
+# b = 6-2
+# c = 2*2
+# d = 10/2
+# j = 10%3
+# print(a,b,c,d,j)
+
+# task1
+# a = 1
+# b = '1'
+# b = float(b)
+# c = 1.14
+# d = a + b + c
+# print(round(d, 2))
+
+	
+
