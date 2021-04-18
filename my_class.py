2a3a62d728f72bc6e1f83a8cb3bb617806296288
Nur <96_nur@mail.ru>
2021-04-18 21:30:06 +0600

    final

---
+++ my_class.py
@@ -0,0 +1,20 @@
+
+class Animals():
+  
+  def __init__(self, kind, sound):
+    self.kind = kind
+    self.sound = sound
+
+  def __cat__(self):
+    print()
+    print('meow')
+
+  def dog():
+    print('vof')
+
+  def say_type(self):
+    print('this is ', self.kind)
+
+
+class newClass(Animals):
+  pass
