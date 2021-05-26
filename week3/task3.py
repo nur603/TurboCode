9e6956842b0150a52d91a5b273a041e772de8f96
Nurzhan Pernebayev <nurzhan.pernebayev@corp.yurta.me>
2021-05-26 18:00:51 +0600

    pochti

--- week3/task3.py
+++ week3/task3.py
@@ -3,10 +3,10 @@ from bs4 import BeautifulSoup
 url = 'https://kolesa.kz/cars/sedan/almaty/?mark-country=12&auto-sweel=1&auto-car-order=1&year[from]=2010&year[to]=2012'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'lxml')
-kolesa = soup.find_all('a',class_='list-link ddl_product_link')
+kolesa = soup.find_all('span',class_='a-el-info-title')
 for i in kolesa:
-	link = i.get('data-product-id')
-	print(i)
+	bsoup = i.find('a',class_='list-link ddl_product_link')
+	link = bsoup.get('data-product-id')
 	url = str('https://kolesa.kz/a/show/') + link
 	response = requests.get(url)
 	soup = BeautifulSoup(response.text, 'lxml')
@@ -15,6 +15,6 @@ for i in kolesa:
 	name = soup.find( 'span', itemprop='name').text.strip()
 	if cheaper != None:
 		cheaper = soup.find('span', class_='cheaper').text.strip()
-		# print(f'{brand} {name} {cheaper}')
+		print(f'{brand} {name} {cheaper}')
 
 
